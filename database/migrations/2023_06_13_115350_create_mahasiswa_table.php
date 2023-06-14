b953a1ecb3ed28e841bd4206173a1bdc4ecbbba0
Orii1 <farisansori231005@gmail.com>
2023-06-14 10:02:08 +0800

    Nambah File

---
+++ database/migrations/2023_06_13_115350_create_mahasiswa_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->nim();
+            $table->string('nama');
+            $table->string('jenis_kelamin');
+            $table->date('tanggal_lahir');  
+            $table->string('nohp');
+            $table->string('email');
+            $table->string('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+};
